0771990e38b09a0f6bd22bb304ecd7fa68ef96ce
Sudesh Chaudhary <48723832+sudesh1122@users.noreply.github.com>
2019-11-07 00:08:41 +0530

    Update PlotStVenantTheory.m

--- PlotStVenantTheory.m
+++ PlotStVenantTheory.m
@@ -1,17 +1,24 @@
-function PlotStVenantTheory(r,v)
+function PlotStVenantTheory(r,v) % defining function to plot maximum principal strain theory 
 % defining points 
-% these points are the point of intersection of given equcations  
-% y = x/v +?/v; y = x/v - ?/v;
-% y = v*x +?; y = v*x - ?;
-x1= (-1)*r/(1+v);
-x2= (-1)*r/(1-v);
-x3= r/(1+v);
-x4= r*(1+v^2)/(1-v^2);
-
-y1= r/(1+v);
-y2= (-1)*r/(1-v);
-y3= (-1)*r/(1+v);
-y4= r*(v^3-v^2+v+1)/(1-v^2);
+% these points are the point of intersection of these equcations:-
+% sigma1 = sigma0 + v*sigma2  --eqn1
+% sigma2 = sigma0 + v*sigma1  --eqn2
+% sigma1 = -sigma0 + v*sigma2 --eqn3
+% sigma2 = -sigma0 + v*sigma1 --eqn4
+pt1 = r/(1-v);
+pt2 = r/(1+v);
+% by eqn1 & eqn2
+x1 = pt1;
+y1 = pt1;
+% by eqn2 and eqn3
+x2 = (-1)*pt2;
+y2 = pt2;
+% by eqn3 and eqn4
+x3 = (-1)*pt1;
+y3 = (-1)*pt1;
+% by eqn4 and eqn1
+x4 =  pt2;
+y4 = (-1)*pt2;
 
 x = [x1, x2, x3, x4, x1];
 y = [y1, y2, y3, y4, y1];
@@ -30,8 +37,8 @@ grid minor
 % setting titlegit push
 title('St Venaant Theory')
 % setting label of x and y axis
-xlabel('?1')
-ylabel('?2')
+xlabel('Sigma1')
+ylabel('Sigma2')
 
 
 
